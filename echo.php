456797391428f5c19330727c64639c9c9b3e95f3
souma-9 <soumacloud@gmail.com>
2020-10-03 09:25:53 +0900

    こんにちはをおはようございますに変更

---
+++ echo.php
@@ -0,0 +1,13 @@
+<?php $word = 'おはようございます'; ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="author" content="井上">
+<title>出力</title>
+</head>
+<body>
+<?php echo $word; ?>
+</body>
+</html>
