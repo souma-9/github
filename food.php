d885bef80a69d723d0b012a4a24a9b53bebd16a6
souma-9 <soumacloud@gmail.com>
2020-10-03 09:19:55 +0900

    肉を果物に変更

---
+++ food.php
@@ -0,0 +1,13 @@
+<?php $food = '果物'; ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="author" content="井上">
+<title>出力</title>
+</head>
+<body>
+<?php echo $food; ?>
+</body>
+</html>
